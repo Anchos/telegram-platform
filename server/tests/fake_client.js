d97d3500795b77d5a38b9df806a09891b73280f6
petr <pd.stroganov@gmail.com>
2018-04-02 13:02:43 +0100

    Restructure

--- server/tests/fake_client.js
+++ server/tests/fake_client.js
@@ -6,7 +6,7 @@ let log = (t, err) => {
     document.getElementById('console').appendChild(e)
 };
 
-let url = 'ws://localhost:5000/client';
+let url = 'ws://0.0.0.0:5000/client';
 let ws = new WebSocket(url);
 
 ws.onopen = () => log(`Connected to ${url}`);
