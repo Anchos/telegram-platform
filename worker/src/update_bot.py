5597cf1f85e17c4ac75347bace699605c1ac58fa
Andrii Kulikov <jadecoresky@gmail.com>
2018-05-20 21:42:17 +0300

    Replaced ternars with dict get method

--- worker/src/update_bot.py
+++ worker/src/update_bot.py
@@ -50,7 +50,7 @@ class UpdateBot(BaseWorker):
                 bot_token=self.get_bot_token(),
                 file_id=chat["photo"]["big_file_id"]
             ),
-            "description": chat["description"] if "description" in chat else "",
+            "description": chat.get("description", ""),
             "members": members
         }
 
