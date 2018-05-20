35dcd3df4fe9929a525d8c7cd934bee62892f7f3
Andrii Kulikov <jadecoresky@gmail.com>
2018-05-20 23:48:42 +0300

    Fixed imports

--- server/migrations/mg_tag_category.py
+++ server/migrations/mg_tag_category.py
@@ -1,5 +1,11 @@
 import json
 
+import sys
+
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from peewee import PostgresqlDatabase
 from playhouse.migrate import migrate, PostgresqlMigrator
 
