2d87893c465cc908175f6b7ea60675c14a1e60c0
petr <pd.stroganov@gmail.com>
2018-05-31 12:34:15 +0100

    Typo fix

--- nginx/setup.sh
+++ nginx/setup.sh
@@ -1,5 +1,5 @@
 /etc/dehydrated-master/dehydrated -c --accept-terms
 
-rm -rf /etc/nginx/conf.d/default.conf && mv /default.nginx /etc/nginx/conf.d/default.conf
+rm -rf /etc/nginx/conf.d/default.conf && mv /default.conf /etc/nginx/conf.d/default.conf
 
 service nginx reload
