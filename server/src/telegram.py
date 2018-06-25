be09c04ca80bcb31418e80abb63fa92b31f559a4
petr <pd.stroganov@gmail.com>
2018-06-25 11:51:35 +0100

    Changed our image hosting system

--- server/src/telegram.py
+++ server/src/telegram.py
@@ -1,12 +1,18 @@
 import json
+import logging
 import random
-import secrets
 from io import BytesIO
 
 import aiohttp
 
+session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False))
+
 
 class Telegram(object):
+    @staticmethod
+    def _log(message: str):
+        logging.info(f"[TELEGRAM] {message}")
+
     @staticmethod
     def get_bot_token() -> str:
         file = open("config.json")
@@ -16,12 +22,9 @@ class Telegram(object):
 
     @staticmethod
     async def send_telegram_request(bot_token: str, method: str, payload: dict) -> dict:
-        session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False))
-
-        async with session:
-            url = "https://api.telegram.org/bot{0}/{1}".format(bot_token, method)
-            async with session.get(url=url, data=payload) as response:
-                return await response.json()
+        url = f"https://api.telegram.org/bot{bot_token}/{method}"
+        async with session.get(url=url, data=payload) as response:
+            return await response.json()
 
     @staticmethod
     async def get_user_profile_photo(bot_token: str, user_id: int) -> str:
@@ -38,20 +41,18 @@ class Telegram(object):
 
     @staticmethod
     async def get_telegram_file(bot_token: str, file_id: id) -> str:
-        session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False))
-
-        file_path = await Telegram.send_telegram_request(
+        file_path = (await Telegram.send_telegram_request(
             bot_token=bot_token,
             method="getFile",
             payload={"file_id": file_id}
-        )
+        ))["result"]["file_path"]
 
-        async with session:
-            url = "https://api.telegram.org/file/bot{0}/{1}".format(bot_token, file_path)
-            async with session.get(url=url) as response:
-                bytes_file = BytesIO(await response.read())
+        url = f"https://api.telegram.org/file/bot{bot_token}/{file_path}"
+        async with session.get(url=url) as response:
+            bytes_file = BytesIO(await response.read())
 
-            form = aiohttp.FormData(quote_fields=False)
-            form.add_field(secrets.token_urlsafe(8), bytes_file, filename="file.png", content_type="image/png")
-            async with session.post(url="http://telegra.ph/upload", data=form) as response:
-                return "https://telegra.ph" + (await response.json())[0]["src"]
+        form = aiohttp.FormData()
+        form.add_field("file", bytes_file)
+
+        async with session.post(url="https://imagebin.ca/upload.php", data=form) as response:
+            return (await response.text()).split("url:")[1].strip()
